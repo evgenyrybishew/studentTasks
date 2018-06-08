dcef171df9f91d2c9684c1e6a87e0f1213cbf939
Evgeny Ryabyshew <ru.evgeny.dev@gmail.com>
2018-06-09 02:01:27 +0300

    implement getWordCounts() method

---
+++ modules/OOP/ArrayVector/src/main/java/ru.edu.java.tasks/WorkCounterImpTest.java
@@ -0,0 +1,97 @@
+package ru.edu.java.tasks;
+
+import org.junit.Assert;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WorkCounterImpTest {
+
+    @org.junit.Test
+    public void setGetTextNullTest() {
+        WordCounter text = new WordCounterImp();
+        Assert.assertNull(text.getText());
+        text.setText(null);
+        Assert.assertNull(text.getText());
+    }
+
+    @org.junit.Test
+    public void setGetText() {
+        WordCounter text = new WordCounterImp();
+        text.setText("This version of the GNU Lesser General Public License incorporates\n" +
+                "the terms and conditions of version 3 of the GNU General Public\n" +
+                "License, supplemented by the additional permissions listed below.");
+
+        Assert.assertTrue(text.getText().equals("This version of the GNU Lesser General Public License incorporates\n" +
+                "the terms and conditions of version 3 of the GNU General Public\n" +
+                "License, supplemented by the additional permissions listed below."));
+
+    }
+
+    @org.junit.Test
+    public void getWordCounts() {
+
+        WordCounter counter = new WordCounterImp();
+        counter.setText("Hello    world 4or  &gt;    r  red yellow green #FFF   &lt;  1. \"123 \", \"Mother\" 2. \"мыла\", 3. <i>a<i>" +
+                "world mother hello green мыла");
+
+
+        Map<String, Long> reg = new HashMap<>();
+        reg.put("hello", 2l);
+        reg.put("world", 2l);
+        reg.put("4or", 1l);
+        reg.put("red", 1l);
+        reg.put("fff", 1l);
+        reg.put("mother", 2l);
+        reg.put("мыла", 2l);
+        reg.put("123", 1l);
+        reg.put("green", 2l);
+        reg.put("yellow", 1l);
+
+        Set<Map.Entry<String, Long>> set = counter.getWordCounts().entrySet();
+        Set<Map.Entry<String, Long>> regSet = reg.entrySet();
+        Assert.assertTrue(set.equals(regSet));
+
+    }
+
+    @org.junit.Test
+    public void getWordCountsSorted() {
+    }
+
+    @org.junit.Test
+    public void sortWordCounts() {
+    }
+
+
+    public static void main(String[] args) {
+//        Pattern pattern = Pattern.compile("([a-z])");
+//
+//        String[] words = pattern.split("Hello    world 4or  &gt;   r  red yellow green #FFF   &lt;  1. \"123 \", \"Mother\" 2. \"мылы\", 3. <i>a<i>" +
+//                "world mother hello green мыла");
+//
+//
+//
+//        for(String word:words)
+//            System.out.println(word);
+
+
+        String input = "Hello    world 4or  &gt;   r  red yellow green #FFF   &lt;  1. \"123 \", \"Mother\" 2. \"мылы\", 3. <i>a<i>" +
+                "world mother hello green мыла жава";
+
+        Pattern pattern = Pattern.compile("(?U)(\\w*)");
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()) {
+
+            if (matcher.group().length() <= 2)
+                continue;
+
+            System.out.println(matcher.group());
+        }
+    }
+
+
+}
---
+++ modules/TextHandling/WordCounter/src/main/java/ru.edu.java.tasks/WordCounterImp.java
@@ -0,0 +1,64 @@
+package ru.edu.java.tasks;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WordCounterImp implements WordCounter {
+
+
+    private Map<String, Long> counter = null;
+    private String text = null;
+
+
+    @Override
+    public void setText(String text) {
+        this.text = text;
+    }
+
+
+    @Override
+    public String getText() {
+        return this.text;
+    }
+
+    @Override
+    public Map<String, Long> getWordCounts() throws IllegalStateException {
+
+        if(this.text == null)
+            return null;
+
+        if (counter == null)
+            this.counter = new HashMap<String, Long>();
+
+        Pattern pattern = Pattern.compile("(?U)(\\w*)");
+        Matcher matcher = pattern.matcher(this.text);
+
+        while(matcher.find()) {
+            if (matcher.group().length() <= 2)
+                continue;
+
+            String word = matcher.group().toLowerCase();
+
+            if (this.counter.containsKey(word)) {
+                long value = this.counter.get(word);
+                this.counter.put(word, ++value);
+            } else
+                this.counter.put(word, 1l);
+        }
+        return this.counter;
+    }
+
+
+    @Override
+    public List<Map.Entry<String, Long>> getWordCountsSorted() throws IllegalStateException {
+        return null;
+    }
+
+    @Override
+    public List<Map.Entry<String, Long>> sortWordCounts(Map<String, Long> orig) {
+        return null;
+    }
+}
