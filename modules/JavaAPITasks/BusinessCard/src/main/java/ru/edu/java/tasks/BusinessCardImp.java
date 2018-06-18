a7a07b86f5677994dcebafcf695ceb9dfb29ca03
Evgeny Ryabyshew <ru.evgeny.dev@gmail.com>
2018-06-18 21:58:35 +0300

    implement task BusinessCard & +testes

---
+++ modules/JavaAPITasks/BusinessCard/src/main/java/ru/edu/java/tasks/BusinessCardImp.java
@@ -0,0 +1,106 @@
+package ru.edu.java.tasks;
+
+import java.util.*;
+
+public class BusinessCardImp implements BusinessCard {
+
+
+    private String name;
+    private String lastname;
+    private String department;
+    private Calendar birthDate;
+    private String gender;
+    private int salary;
+    private String tel;
+
+    public BusinessCardImp(){}
+
+    public BusinessCardImp(String name, String lastname, String department, Calendar birthDate, String gender, int salary, String tel) {
+        this.name = name;
+        this.lastname = lastname;
+        this.department = department;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.salary = salary;
+        this.tel = tel;
+    }
+
+    @Override
+    public BusinessCard getBusinessCard(Scanner scanner) throws NoSuchElementException, InputMismatchException {
+        final int FIELDS = 7;
+        String[] data = scanner.nextLine().split(";");
+        if (data.length == FIELDS) {
+            this.name = data[0];
+            this.lastname = data[1];
+            this.department = data[2];
+
+            String[] date = data[3].split("-");
+            birthDate = new GregorianCalendar(Integer.valueOf(date[2]),
+                    Integer.valueOf(date[1]), Integer.valueOf(date[0]));
+
+            if (data[4].equals("M") || data[4].equals("F"))
+                this.gender = data[4];
+            else
+                throw new NoSuchElementException("Invalid value for gender!");
+
+            int checkSalary = Integer.valueOf(data[5]);
+            if(checkSalary < 100 || checkSalary > 100000)
+                throw new NoSuchElementException("Invalid value for salary!");
+            this.salary = checkSalary;
+
+            if(data[6].length() != 10 || !isDigit(data[6]))
+                throw new NoSuchElementException("Invalid value for phone!");
+            this.tel = data[6];
+            return  this;
+        }
+        throw new InputMismatchException();
+    }
+
+
+    private boolean isDigit(String input){
+
+        try{
+            Integer.valueOf(input);
+            return true;
+        }catch (Exception e){
+            return  false;
+        }
+    }
+
+
+    @Override
+    public String getEmployee() {
+        return "Employee: " + this.name + " " + this.lastname + "\n"+
+                "Salary: " + this.salary + "\n"+
+                "Age: " + this.getAge() + "\n" +
+                "Gender: " + this.getGender() + "\n" +
+                "Phone: " + this.getPhoneNumber();
+
+    }
+
+    @Override
+    public String getDepartment() {
+        return this.department;
+    }
+
+    @Override
+    public int getSalary() {
+        return this.salary;
+    }
+
+    @Override
+    public int getAge() {
+        return Calendar.getInstance().get(Calendar.YEAR) - this.birthDate.get(Calendar.YEAR);
+    }
+
+    @Override
+    public String getGender() {
+        return this.gender.equals("M") ? "Male" : "Female";
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        return "+7 " + this.tel.substring(0, 3) + "-" + this.tel.substring(3,6) + "-" + this.tel.substring(6,8) + "-" + tel.substring(8);
+    }
+
+}
---
+++ modules/JavaAPITasks/BusinessCard/src/main/java/ru/edu/java/tasks/BusinessCardImpTest.java
@@ -0,0 +1,173 @@
+package ru.edu.java.tasks;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.*;
+
+public class BusinessCardImpTest {
+
+
+    @Test
+    public void getBusinessCardTest() throws Exception {
+
+        BusinessCard card = new BusinessCardImp();
+        Scanner in = new Scanner(new File("studentTasks/modules/JavaAPITasks/BusinessCard/src/main/java/ru/edu/java/tasks/test.txt"));
+        card.getBusinessCard(in);
+
+        Field field = card.getClass().getDeclaredField("name");
+        field.setAccessible(true);
+        String temp = (String) field.get(card);
+        Assert.assertTrue(temp.equals("Chuck"));
+
+        field = card.getClass().getDeclaredField("lastname");
+        field.setAccessible(true);
+        temp = (String) field.get(card);
+        Assert.assertTrue(temp.equals("Norris"));
+
+        field = card.getClass().getDeclaredField("department");
+        field.setAccessible(true);
+        temp = (String) field.get(card);
+        Assert.assertTrue(temp.equals("DSI"));
+
+        field = card.getClass().getDeclaredField("birthDate");
+        field.setAccessible(true);
+        Calendar bd = (Calendar) field.get(card);
+
+        Assert.assertEquals(bd.get(Calendar.DAY_OF_MONTH), 10);
+        Assert.assertEquals(bd.get(Calendar.MONTH), 4);
+        Assert.assertEquals(bd.get(Calendar.YEAR), 1940);
+
+        field = card.getClass().getDeclaredField("gender");
+        field.setAccessible(true);
+        temp = (String) field.get(card);
+        Assert.assertTrue(temp.equals("M"));
+
+        field = card.getClass().getDeclaredField("salary");
+        field.setAccessible(true);
+        int salary = (int) field.get(card);
+        Assert.assertEquals(1000, salary);
+
+        field = card.getClass().getDeclaredField("tel");
+        field.setAccessible(true);
+        temp = (String) field.get(card);
+        Assert.assertTrue(temp.equals("1234567890"));
+    }
+
+
+    @Test
+    public void getBusinessCardTestWrongTel() throws Exception {
+        BusinessCard card = new BusinessCardImp();
+        Scanner in = new Scanner(new File("/Users/mac/Developer/Projects/Java Projects/studentTasks/modules/JavaAPITasks/BusinessCard/src/main/java/ru/edu/java/tasks/wrong1.txt"));
+        boolean wrong = false;
+        try {
+            card.getBusinessCard(in);
+        } catch (NoSuchElementException e) {
+            wrong = true;
+        }
+        Assert.assertTrue(wrong);
+    }
+
+    @Test
+    public void getBusinessCardTestWrongSalary() throws Exception {
+        BusinessCard card = new BusinessCardImp();
+        Scanner in = new Scanner(new File("/Users/mac/Developer/Projects/Java Projects/studentTasks/modules/JavaAPITasks/BusinessCard/src/main/java/ru/edu/java/tasks/wrong2.txt"));
+        boolean wrong = false;
+        try {
+            card.getBusinessCard(in);
+        } catch (NoSuchElementException e) {
+            wrong = true;
+        }
+        Assert.assertTrue(wrong);
+    }
+
+    @Test
+    public void getBusinessCardTestWrongGender() throws Exception {
+        BusinessCard card = new BusinessCardImp();
+        Scanner in = new Scanner(new File("/Users/mac/Developer/Projects/Java Projects/studentTasks/modules/JavaAPITasks/BusinessCard/src/main/java/ru/edu/java/tasks/wrong3.txt"));
+        boolean wrong = false;
+        try {
+            card.getBusinessCard(in);
+        } catch (NoSuchElementException e) {
+            wrong = true;
+        }
+        Assert.assertTrue(wrong);
+    }
+
+    @Test
+    public void getBusinessCardTestnIputMismatchException() throws Exception {
+        BusinessCard card = new BusinessCardImp();
+        Scanner in = new Scanner(new File("/Users/mac/Developer/Projects/Java Projects/studentTasks/modules/JavaAPITasks/BusinessCard/src/main/java/ru/edu/java/tasks/wrong4.txt"));
+        boolean wrong = false;
+        try {
+            card.getBusinessCard(in);
+        } catch (InputMismatchException e) {
+            wrong = true;
+        }
+        Assert.assertTrue(wrong);
+    }
+
+
+    @Test
+    public void getEmployeeTest() {
+        BusinessCard card = new BusinessCardImp("Chuck", "Norris", "DSI",
+                new GregorianCalendar(1940, 4, 10),
+                "M", 1000, "1234567890");
+
+        Assert.assertTrue(card.getEmployee().equals("Employee: Chuck Norris\n" +
+                "Salary: 1000\n" +
+                "Age: 78\n" +
+                "Gender: Male\n" +
+                "Phone: +7 123-456-78-90"));
+    }
+
+    @Test
+    public void getDepartmentTest() {
+        BusinessCard card = new BusinessCardImp("Chuck", "Norris", "DSI",
+                new GregorianCalendar(1940, 4, 10),
+                "M", 1000, "1234567890");
+
+        Assert.assertTrue(card.getDepartment().equals("DSI"));
+    }
+
+    @Test
+    public void getSalaryTest() {
+        BusinessCard card = new BusinessCardImp("Chuck", "Norris", "DSI",
+                new GregorianCalendar(1940, 4, 10),
+                "M", 1000, "1234567890");
+
+        Assert.assertEquals(1000, card.getSalary());
+    }
+
+    @Test
+    public void getAgeTest() {
+        BusinessCard card = new BusinessCardImp("Chuck", "Norris", "DSI",
+                new GregorianCalendar(1940, 4, 10),
+                "M", 1000, "1234567890");
+        Assert.assertEquals(78, card.getAge());
+
+
+    }
+
+    @Test
+    public void getGenderTest() {
+        BusinessCard card = new BusinessCardImp("Chuck", "Norris", "DSI",
+                new GregorianCalendar(1940, 4, 10),
+                "M", 1000, "1234567890");
+
+        Assert.assertTrue(card.getGender().equals("Male"));
+    }
+
+    @Test
+    public void getPhoneNumberTest() {
+
+        BusinessCard card = new BusinessCardImp("Chuck", "Norris", "DSI",
+                new GregorianCalendar(1940, 4, 10),
+                "M", 1000, "1234567890");
+
+        Assert.assertTrue(card.getPhoneNumber().equals("+7 123-456-78-90"));
+
+    }
+}
